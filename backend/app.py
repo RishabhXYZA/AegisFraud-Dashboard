d835efc86b2808b0ad85926b825d1c3c9b8eb422
Rishabh Bhasin <rishabhgndu2023@gmail.com>
2026-08-28 18:19:18 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,7 @@
 
 import os
 from flask import Flask, render_template, jsonify, request
-from config import Config
+from backend.config import Config
 import database
 import query_registry
 import analytics
